b394927683908bb05c0229d07b81933ccf2dacb3
Daniil Tarasenko <tarasenko_dd_25@student.grsu.by>
2025-10-27 22:46:53 +0300

    Add files via upload
    добавил A2.py

---
+++ A2.py
@@ -0,0 +1,29 @@
+rows = int(input("Введите rows: "))
+columns = int(input("Введите columns: "))
+ch = input("Введите ch: ")
+
+def draw_rectangle(rows, columns, ch):
+    for i in range(rows):
+        for j in range(columns):
+            print(ch, end=" ")
+        print()
+
+def draw_triangle(rows, ch):
+    for i in range(rows +1):
+        for j in range(i):       
+            print(ch, end=" ")
+        print()
+
+
+def draw_frame(rows, columns, ch):
+    for i in range(rows):
+        for j in range(columns):
+            if i == 0 or i == rows - 1  or j == 0 or j == columns - 1:
+                print(ch, end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+draw_rectangle(rows, columns, ch)
+draw_triangle(rows, ch)
+draw_frame(rows, columns, ch)
