4cc36a9ec32155faf9dabc222d73e158a8415cc3
Daniil Tarasenko <tarasenko_dd_25@student.grsu.by>
2025-10-27 22:45:04 +0300

    Add files via upload
    добавил A1.py

---
+++ A1.py
@@ -0,0 +1,31 @@
+import time
+import random
+totl_time = 0
+right_answer = 0
+N = int(input("Количество строк: "))
+for i in range(N):
+    print(f"{i + 1}/{N}")
+    a = random.randint(2, 9)
+    b = random.randint(2, 9)
+    while True:
+        try:
+            start_time = time.time()
+            answer = int(input(f"{a} × {b} = "))
+            time_spend = time.time() - start_time
+            break
+        except ValueError:
+            print("Пожалуйста, введите целое число!")
+    totl_time += time_spend
+    if answer == a*b:
+        right_answer+=1
+        print(f"Верно(Время:{time_spend:.1f}сек.)")
+    elif answer != a*b:
+        print(f"Неверно! Правильно: {a*b}(Время:{time_spend:.1f}сек.)")
+print(f"=" * 50)
+print("СТАТИСТИКА:")
+print(f"=" * 50)
+print(f"Общее время:{totl_time:.1f}")
+print(f"Cреднее время на вопрос:{totl_time / N:.1f}")
+print(f'Правильных ответов:{right_answer}/{N}')
+print(f"Процент правельных: {right_answer*100 / N}%")
+
